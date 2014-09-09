4123f7e7229df18147ca42e699b44dcafbdd4a0d
Chad Brading <chadbrading@gmx.com>
2014-09-09 09:50:21 -0600

    updates admin view spec to supplier view spec

---
+++ spec/features/supplier_view_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+require 'capybara/rails'
+require 'capybara/rspec'
+
+describe 'a supplier viewing the items page', type: :feature do
+  let(:user) do
+    User.create!(:full_name   => "john doe",
+                :email        => "john_doe@example.com",
+                :display_name => "john_doe_123",
+                :role         => "supplier",
+                :password     => 'password')
+  end
+
+  context "supplier user functionality" do
+    it 'can login with the correct information' do
+      page.visit signin_path
+      page.fill_in('session[email]',    with: user.email)
+      page.fill_in('session[password]', with: 'password')
+      page.click_button('Sign in')
+      save_and_open_page
+      expect(page.current_path).to eq(supplier_dashboard_path)
+    end
+
+    it 'can destroy and item' do
+      item = Item.create!(title: "Cami's", inventory: 12, price_pie: 30.99,
+                          description: "yummy")
+
+      page.visit signin_path
+      page.fill_in('session[email]',    with: user.email)
+      page.fill_in('session[password]', with: 'password')
+      page.click_button('Sign in')
+      page.click_link('Menu Item Management')
+      expect(page).to have_content("yummy")
+      page.click_link('Destroy')
+      expect(page).to_not have_content("yummy")
+    end
+  end
+end
