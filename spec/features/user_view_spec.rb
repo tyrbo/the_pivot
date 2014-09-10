c9bf6b192e244fe427d46accc9cbd1621369a3a4
Tan Doan <tdoan87@gmail.com>
2014-09-10 14:11:00 -0600

    implements flash message for type of user after registration

--- spec/features/user_view_spec.rb
+++ spec/features/user_view_spec.rb
@@ -21,5 +21,19 @@ describe 'user view', type: :feature do
       expect(page).to have_content("Thanks, your request is pending!")
       expect(current_path).to eq(root_path)
     end
+
+    it 'unregistered user can register as a provider' do
+      click_link_or_button('Register')
+      fill_in("Full name", with: "Chad Brading")
+      fill_in("Email", with: "chad@example.com")
+      fill_in("Display name", with: "chad@example.com")
+      fill_in("Password", with: "provider123")
+      fill_in("Confirmation", with: "provider123")
+      choose("user_role_provider")
+      click_button("Create Account")
+
+      expect(page).to have_css("#provider-thanks")
+      expect(current_path).to eq(root_path)
+    end
   end
 end
