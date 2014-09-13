da38365cdc5185e18efdb0b9f3870ee759467a97
Tan Doan <tdoan87@gmail.com>
2014-09-13 15:12:48 -0600

    user can view order detail and have suppliers displayed, close #61

--- spec/features/user_can_visit_dashboard_spec.rb
+++ spec/features/user_can_visit_dashboard_spec.rb
@@ -12,5 +12,26 @@ describe 'Viewing the dashboard', type: :feature do
 
       expect(page).to have_content order.order_total
     end
+
+    it 'can link to individual orders' do
+      order = FactoryGirl.create(:order, user: user)
+
+      login_as(username: user.email, password: 'password')
+      click_link 'Dashboard'
+      click_link 'View Order Details'
+
+      expect(current_path).to eq(order_path(order))
+    end
+
+    it 'shows supplier for each item on details page' do
+      order = FactoryGirl.create(:order, user: user)
+
+      login_as(username: user.email, password: 'password')
+      click_link 'Dashboard'
+      click_link 'View Order Details'
+      within('#order-items-header') do
+        expect(page).to have_content('Supplier')
+      end
+    end
   end
 end
