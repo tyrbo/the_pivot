265db514b5970f08ad5c371fd58d6b540a0da2a2
Chad Brading <chadbrading@gmx.com>
2014-09-16 15:47:19 -0600

    only allows one add to cart button to be displayed at once

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,7 +6,16 @@
 
 $(document).ready(function() {
   $(".cart-link").click(function(){
-    $(this).closest("tr").next().next().toggleClass('hidden');
+    var nextCartInfo = $(this).closest('tr').next().next();
+    var allCartInfo = $(this).closest("div.align-to-sidebar").find(".cart-info");
+
+
+    if (nextCartInfo.hasClass('hidden')) {
+      allCartInfo.addClass('hidden');
+      nextCartInfo.toggleClass('hidden');
+    } else {
+      allCartInfo.addClass('hidden');
+    }
   });
 
   $(".details-link").click(function(){
