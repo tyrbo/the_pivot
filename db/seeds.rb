4fc85f0678c014472bfec35c07448be4d65ca1dc
Chad Brading <chadbrading@gmx.com>
2014-09-16 09:31:41 -0600

    updates seed data for suppliers

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,59 +7,64 @@ Category.create(name: "Clothing")
 User.create(full_name: "Rachel Warbelow", email: "demo+rachel@jumpstartlab.com", password: "password", role: :user, display_name: nil)
 User.create(full_name: "Jeff", email: "demo+jeff@jumpstartlab.com", password: "password", role: :user, display_name: "j3")
 User.create(full_name: "Jorge Tellez", email: "demo+jorge@jumpstartlab.com", password: "password", role: :user, display_name: "novohispano")
-@josh = User.create(full_name: "Josh Cheek", email: "demojosh@jumpstartlab.com", password: "password", password_confirmation: "password", display_name: "Programmer Relief Foundation", role: :supplier)
-@tan = User.create(full_name: "Tan Doan", email: "tandoan@example.com", password: "password", password_confirmation: "password", display_name: "Viet Cong Rescue Crew", role: :supplier)
+josh = User.create(full_name: "Josh Cheek", email: "demojosh@jumpstartlab.com", password: "password", password_confirmation: "password", display_name: "Programmer Relief Foundation", role: :supplier)
+tan = User.create(full_name: "Tan Doan", email: "tandoan@example.com", password: "password", password_confirmation: "password", display_name: "Viet Cong Rescue Crew", role: :supplier)
 
+kids = Supplier.create(name: 'Help the Kids', url: 'http://wwww.savethechildren.org', description: 'One kid at a time')
+tsunami = Supplier.create(name: 'Tsunami Relief', url: "http://www.globalgiving.org/projects/japan-earthquake-tsunami-relief/", description: 'Preventing Wipe Outs')
 
-@josh.items.create(title: "Bandage", description: "Waterproof Standard Size",
+SuppliersUser.create(supplier_id: kids.id, user_id: josh.id)
+SuppliersUser.create(supplier_id: tsunami.id, user_id: tan.id)
+
+kids.items.create(title: "Bandage", description: "Waterproof Standard Size",
  inventory: 10000000, price: 12.99, size: '400', category_ids: [1])
-@josh.items.create(title: "Ibuprofin", description: "200ml, 200 per bottle",
+kids.items.create(title: "Ibuprofin", description: "200ml, 200 per bottle",
  inventory:12000000, price: 17.99, size: '200', category_ids:[1])
-@josh.items.create(title: "Gauze", description: "Cloth",
+kids.items.create(title: "Gauze", description: "Cloth",
  inventory:3000000, price: 23.99, size: '200', category_ids:[1])
-@josh.items.create(title: "Neosporin", description: "Fix your cuts",
+kids.items.create(title: "Neosporin", description: "Fix your cuts",
  inventory:5000000, price: 23.99, size: '200', category_ids:[1])
-@josh.items.create(title: "Antacids", description: "Fix your stomach",
+kids.items.create(title: "Antacids", description: "Fix your stomach",
  inventory: 4000000, price: 29.99, size: '200', category_ids: [1])
-@josh.items.create(title: "Water Bottles", description: "32 pack",
+kids.items.create(title: "Water Bottles", description: "32 pack",
  inventory: 5000000, price: 35.99, size: '200', category_ids:[2])
-@josh.items.create(title: "Soup", description: "10pac asorted types",
+kids.items.create(title: "Soup", description: "10pac asorted types",
  inventory: 10000000, price: 33.99, size: '200', category_ids: [2])
-@josh.items.create(title: "Rice", description: "50 lb bag",
+kids.items.create(title: "Rice", description: "50 lb bag",
  inventory: 10000000, price: 33.99, size: '200', category_ids:[2])
-@josh.items.create(title: "Protien Bars", description: "30 boxes",
+kids.items.create(title: "Protien Bars", description: "30 boxes",
  inventory: 12000000, price: 31.15, size: '200', category_ids:[2])
-@josh.items.create(title: "Apples", description: "Red",
+kids.items.create(title: "Apples", description: "Red",
  inventory: 12000000, price: 36.99, size: '200', category_ids:[2])
-@josh.items.create(title: "Rope", description: "Tie to things",
+kids.items.create(title: "Rope", description: "Tie to things",
  inventory: 15000000, price: 30.99, size: '200', category_ids: [3])
-@josh.items.create(title: "Blankets", description: "Keeps warms",
+kids.items.create(title: "Blankets", description: "Keeps warms",
  inventory: 15000000, price: 30.99, size: '200', category_ids:[3])
-@tan.items.create(title: "Tents", description: "Easy to set up",
+tsunami.items.create(title: "Tents", description: "Easy to set up",
  inventory: 5000000, price: 30.99, size: '200', category_ids:[3])
-@tan.items.create(title: "Sleeping Bag", description: "Adult sized",
+tsunami.items.create(title: "Sleeping Bag", description: "Adult sized",
  inventory: 12000000, price: 30.99, size: '200', category_ids:[3])
-@tan.items.create(title: "Cot", description: "Twin Sized Sleeping Cot",
+tsunami.items.create(title: "Cot", description: "Twin Sized Sleeping Cot",
  inventory: 12000000, price: 30.99, size: '200', category_ids:[3])
-@tan.items.create(title: "Van Rental", description: "8 person van per day",
+tsunami.items.create(title: "Van Rental", description: "8 person van per day",
  inventory: 15, price: 30.99, size: '200', category_ids: [4])
-@tan.items.create(title: "Truck Rental", description: "Standard sized pickup truck per day",
+tsunami.items.create(title: "Truck Rental", description: "Standard sized pickup truck per day",
  inventory: 15, price: 30.99, size: '200', category_ids:[4])
-@tan.items.create(title: "Helicopter Pickup", description: "One location",
+tsunami.items.create(title: "Helicopter Pickup", description: "One location",
  inventory: 12000000, price: 30.99, size: '200', category_ids:[4])
-@tan.items.create(title: "Pants", description: "All sizes",
+tsunami.items.create(title: "Pants", description: "All sizes",
  inventory: 12000000, price: 30.99, size: '200', category_ids:[5])
-@tan.items.create(title: "Sweaters", description: "All Sizes",
+tsunami.items.create(title: "Sweaters", description: "All Sizes",
  inventory: 12000000, price: 30.99, size: '200', category_ids:[5])
-@tan.items.create(title: "T-Shirts", description: "All Sizes",
+tsunami.items.create(title: "T-Shirts", description: "All Sizes",
  inventory: 15, price: 30.99, size: '200', category_ids: [5])
-@tan.items.create(title: "Jackets", description: "All Sizes",
+tsunami.items.create(title: "Jackets", description: "All Sizes",
  inventory: 15, price: 30.99, size: '200', category_ids:[5])
-@tan.items.create(title: "Socks", description: "Wool, all sizes",
+tsunami.items.create(title: "Socks", description: "Wool, all sizes",
  inventory: 12000000, price: 30.99, size: '200', category_ids:[5])
-@tan.items.create(title: "Shoes", description: "Sneakers, all sizes",
+tsunami.items.create(title: "Shoes", description: "Sneakers, all sizes",
  inventory: 12000000, price: 30.99, size: '200', category_ids:[5])
-@tan.items.create(title: "Undergarments", description: "All types & sizes",
+tsunami.items.create(title: "Undergarments", description: "All types & sizes",
  inventory: 12000000, price: 30.99, size: '200', category_ids:[5])
 
 Order.create(user_id: 1, order_total: 15, order_type: "pick-up", delivery_address: nil, order_status: "completed")
@@ -84,7 +89,6 @@ OrderItem.create(order_id: 8, item_id: 10)
 OrderItem.create(order_id: 9, item_id: 11)
 OrderItem.create(order_id: 10, item_id: 12)
 
-Supplier.create(name: 'Food For Kids', url: 'http://wwww.savethechildren.org', description: 'One kid at a time')
 
 
 "======= Shit has been seeded ========"
