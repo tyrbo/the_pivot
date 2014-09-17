75c1e22c2cde2a17aa112b1b1e9b9b5d6627169b
Jonmichael Chambers <jon@tyrbo.net>
2014-09-17 11:27:47 -0600

    Changing an env name for paperclip s3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,7 +85,7 @@ Rails.application.configure do
   config.paperclip_defaults = {
   :storage => :s3,
   :s3_credentials => {
-    :bucket => ENV['dinner-dash-assets'],
+    :bucket => ENV['S3_BUCKET_NAME'],
     :access_key_id => ENV['S3_KEY'],
     :secret_access_key => ENV['S3_SECRET']
     }
