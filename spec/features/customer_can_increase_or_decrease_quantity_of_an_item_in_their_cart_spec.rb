e2033004d4af703a15553f0a5e755603f8be38e1
Allison Larson <allisonlarson91@gmail.com>
2014-09-23 14:11:48 -0600

    beginning new quantity adventure

--- spec/features/customer_can_increase_or_decrease_quantity_of_an_item_in_their_cart_spec.rb
+++ spec/features/customer_can_increase_or_decrease_quantity_of_an_item_in_their_cart_spec.rb
@@ -6,20 +6,18 @@ describe 'a user viewing the cart page', type: :feature do
   let!(:supplier) { FactoryGirl.create(:supplier) }
   let!(:keylime) { supplier.items.create!(title: 'Key Lime', description: 'Yummy',  inventory: 100, price: 30.00, categories: [category]) }
 
-  it 'can increase the quantity of an item by one' do
-    pending
+  xit 'can increase the quantity of an item by one' do
     page.visit items_path
-    click_on 'Add to Cart'
-    click_on 'Submit'
+    fill_in('quantity', with: 1)
+    click_on('Add To Cart')
     visit cart_path
     expect(page).to have_content('Key Lime')
-    click_on '+'
-    visit cart_path
+    fill_in('quantity', with: '2')
+    click_on('Update')
     expect(page).to have_content(2)
   end
 
-  it 'can decrease the quantity of an item by one' do
-    pending
+  xit 'can decrease the quantity of an item by one' do
     page.visit items_path
     click_on 'Add to Cart'
     click_on 'Submit'
@@ -31,7 +29,7 @@ describe 'a user viewing the cart page', type: :feature do
     expect(page).to have_content(1)
   end
 
-  it 'can delete an item by setting quantity to zero' do
+  xit 'can delete an item by setting quantity to zero' do
     pending
     page.visit items_path
     click_on 'Add to Cart'
