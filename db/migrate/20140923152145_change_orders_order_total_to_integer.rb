721f1dee1829177eaa12e6c2b81bf86a65f70bb1
Jonmichael Chambers <jon@tyrbo.net>
2014-09-23 10:14:16 -0600

    Adjusting a migration or two

--- db/migrate/20140923152145_change_orders_order_total_to_integer.rb
+++ db/migrate/20140923152145_change_orders_order_total_to_integer.rb
@@ -1,6 +1,6 @@
 class ChangeOrdersOrderTotalToInteger < ActiveRecord::Migration
   def change
-    remove_column :order_items, :order_total, :decimal
+    remove_column :order_items, :order_total, :decimal, { precision: 4, scale: 2 }
     add_column :order_items, :order_total, :integer
   end
 end
