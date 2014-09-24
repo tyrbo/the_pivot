17e1eddf86106482c0fe3d14e45a3b74b80d61b8
Allison Larson <allisonlarson91@gmail.com>
2014-09-24 14:25:37 -0600

    fix address formatting on sub order index

--- app/controllers/dashboard/sub_orders_controller.rb
+++ app/controllers/dashboard/sub_orders_controller.rb
@@ -3,10 +3,11 @@ class Dashboard::SubOrdersController < Dashboard::SupplierController
   def index
     if params[:filter]
       @sub_orders = current_supplier.sub_orders.select do |sub_order|
-        sub_order.status == params[:filter] 
+        sub_order.status == params[:filter]
       end
     else
       @sub_orders = current_supplier.sub_orders
+
     end
   end
 
