d0d24fde93c11082b7491f3caad1c300d207914d
Tan Doan <tdoan87@gmail.com>
2014-09-23 18:33:18 -0600

    user dashboard re-did with tabs

---
+++ old_index.markdown
@@ -0,0 +1,11 @@
+<div class="col-md-10">
+
+
+  
+
+
+
+  <div class="col-md-2">
+    <div class="list-group" id='sidebar-padding'>
+    </div>
+  </div>
--- spec/features/supplier_item_spec.rb
+++ spec/features/supplier_item_spec.rb
@@ -11,7 +11,7 @@ describe 'a supplier viewing the items page', type: :feature do
     before(:each) do
       login
       visit dashboard_root_path
-      page.click_link('Item Management')
+      page.click_link('Listings')
     end
 
     it 'can show an item' do
@@ -24,7 +24,7 @@ describe 'a supplier viewing the items page', type: :feature do
       item = FactoryGirl.create(:item, title: 'Hello', supplier: nil)
 
       visit dashboard_root_path
-      page.click_link('Item Management')
+      page.click_link('Listings')
 
       expect(page).to_not have_content item.title
     end
