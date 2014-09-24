a922424e1587fe866635fde2d422d6ee6cbe678d
Jonmichael Chambers <jon@tyrbo.net>
2014-09-24 12:13:18 -0600

    Finishes resetting passwords

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -7,4 +7,22 @@ class PasswordResetsController < ApplicationController
     user.send_password_reset if user
     redirect_to signin_path, notice: 'You should receive an email soon with instructions for resetting your password.'
   end
+
+  def edit
+    @user = User.find_by(password_reset_token: params[:id])
+
+    unless @user
+      redirect_to new_password_reset_path, notice: 'The reset token you provided is invalid.'
+    end
+  end
+
+  def update
+    @user = User.find_by(password_reset_token: params[:id])
+
+    if @user && @user.update_attributes(params[:user].permit(:password, :password_confirmation))
+      redirect_to signin_path, notice: 'Your password has been reset.'
+    else
+      render :edit
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
   resources :orders, only: [:new, :create]
   resources :users, only: [:create, :new]
   resources :sessions, only: [:new, :create, :destroy]
-  resources :password_resets, only: [:new, :create, :edit]
   resource  :verifications
+  resources :password_resets, only: [:new, :create, :edit, :update]
 
   resources :suppliers, only: [:index, :new, :create, :show] do
     get      'pending_admin'    =>  'suppliers#add_pending_admin'
